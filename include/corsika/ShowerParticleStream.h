77a71990a1a6ab09add9bd8be9d18a96ffc8d7ad
David Delventhal <ddelventhal@icecube.wisc.edu>
2017-03-02 11:52:39 -0600

    Added back header

---
+++ include/corsika/ShowerParticleStream.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <corsika/RawParticleIterator.h>
+#include <boost/optional.hpp>
+
+namespace corsika
+{
+    //
+    // High-Level particle iterator that records the particles' history in there parent and grandparent fields.
+    //
+    struct ShowerParticleStream
+    {
+        ShowerParticleStream(){}
+        ShowerParticleStream(RawStreamPtr stream, size_t start, double timeOffset, int observationLevel, bool keepMuProd);
+        virtual void Rewind() { iterator_->Rewind(); }
+        boost::optional<CorsikaParticle> NextParticle();
+        
+    private:
+        boost::optional<CorsikaParticle> value_;
+        RawParticleIteratorPtr iterator_;
+        double fTimeOffset;
+        int fObservationLevel;
+        bool fKeepMuProd;
+    };
+}
